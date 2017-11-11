66e2980666790c489f4bc91030f6c4c76a6b0853
emiliesedziol <sedziole@gmail.com>
2017-11-11 13:56:47 -0500

    inital commit

---
+++ src/emilie_sedziol_9_1_animal_counter/AnimalCounterApp.java
@@ -0,0 +1,45 @@
+package emilie_sedziol_9_1_animal_counter;
+
+public class AnimalCounterApp {
+
+	public static void main(String[] args) {
+		System.out.println("inside the main app");
+		System.out.println("");
+		
+		Alligator a = new Alligator();
+		
+		count(a, 3);
+		
+		System.out.println("");
+		System.out.println("Sheep");
+		
+		Sheep b = new Sheep();
+		count(b, 2);
+		System.out.println("");
+		
+		try {
+			Sheep b2 = new Sheep();
+			
+			Sheep bs = (Sheep) b2.clone();
+			bs.instanceVariable = "Black Sheep";
+			count(bs, 3);
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+	
+	public static void count(Countable c, int maxCount) {
+		// Alligator a = new Alligator();
+		c.resetCount();
+		
+		for (int i = 1; i <= maxCount; i++ ) {
+			String aString = c.getCountString();
+			System.out.println(aString);
+			
+		}
+	}
+
+}
